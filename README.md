9b707a02ca76a8c3987bf05204b166658ceaf031
Wink Saville <wink@saville.com>
2022-11-16 15:07:41 -0800

    feat: Rc owner with S1::this as RefCell<Option<Weak<Self>>>
    This is kinda like this Rc doc examples:
      https://doc.rust-lang.org/std/rc/#examples
    
    Here owner_s1 has type Rc<S1> and S1::this has type
    RefCell<Option<Weak<S1>>>.

--- README.md
+++ README.md
@@ -1,33 +1,66 @@
 # Experiment with RefCell
 
-By using `RefCell` with `MaybeUninit` I'm trying to make it a
-little more idiomatic rust. In that you use s1.this.borrow().v1()
-instead of a S1::v1(&s1). But it doesn't always work :)
+Here using and `Rc` and `RefCell` and is inspired by
+the [Rc doc example](https://doc.rust-lang.org/std/rc/#examples).
+I've also `impl Display for S1` which displays the address
+of S1, &S1::this, S1::this, &S1::v1, S1::v1, sc (strong_counter)
+and wc (weak_counter).
 
 # Run
 
-Fails dev builds:
+Run dev
 ```
 $ cargo run
    Compiling exper-refcell v0.1.0 (/home/wink/prgs/rust/myrepos/exper-refcell)
-      Finished dev [unoptimized + debuginfo] target(s) in 0.13s
-      Running `/home/wink/prgs/rust/myrepos/exper-refcell/target/debug/exper-refcell`
-s1: 0x7ffc17039928
-self: 0x7ffc17039928 v1: 123
-self: 0x7ffc17039928 v1: 386111784
-Failed
+    Finished dev [unoptimized + debuginfo] target(s) in 0.14s
+     Running `target/debug/exper-refcell`
+offset_of S1.this 0
+offset_of S1.v1 16
+new:- 0x7ffe56111518 S1 { &this=0x7ffe56111518 this=None &v1=0x7ffe56111528 v1=123 }
+main:+ &owner_s1=0x7ffe56111510 owner_s1=0x564a2722faf0 S1 { &this=0x564a2722faf0 this=None &v1=0x564a2722fb00 v1=123 }
+main:  initialize owner_s1: 0x564a2722faf0 S1 { &this=0x564a2722faf0 this=0x564a2722faf0 &v1=0x564a2722fb00 v1=123 sc=1 wc=1 }
+main:  create    owner2_s1: 0x564a2722faf0 S1 { &this=0x564a2722faf0 this=0x564a2722faf0 &v1=0x564a2722fb00 v1=123 sc=2 wc=1 }
+main:  owner_s1.v1_via_this=123
+main:- owner_s1: 0x564a2722faf0 S1 { &this=0x564a2722faf0 this=0x564a2722faf0 &v1=0x564a2722fb00 v1=123 sc=2 wc=1 }
+Drop S1 0x564a2722faf0 S1 { &this=0x564a2722faf0 this=0x564a2722faf0 &v1=0x564a2722fb00 v1=123 sc=0 wc=0 }
 ```
 
-Succeeds release builds:
+With `struct S1` defined as:
+```
+struct S1 {
+    this: RefCell<Option<Weak<Self>>>,
+    v1: i32,
+}
+```
+
+Then from the above output the following is true:
+  * "new:- .." shows that in `new()` `S1` created on the stack at `0x7ffe56111518`
+  * "main:+ &ow.." `&owner_s1=0x7ffe56111510` shows that `owner_s1` is on the stack
+  * "main:+ &ow.." `owner_s1=0x564a2722faf0 S1` shows that the owner_s1 data is on the heap
+  * "main:  init.." `&this=0x564a2722faf0` this is at offset 0, i.e. equals `0x564a2722faf0 + 0x00`
+  * "main:  init.." `this=0x564a2722faf0` points at itself, i.e. `owner_s1=0x564a2722faf0 S1`
+  * "main:  init.." `&v1=0x564a2722fb00` v1 is at offset 16, i.e. equals `0x564a2722faf0 + 0x10`
+  * "main:  init.." `v1=123` is `V1_DEFAULT`
+  * "main:  init.." `sc=1` is strong_count
+  * "main:  init.." `wc=1` is weak_count
+  * "main:  crea.." `sc=2` strong_count was incremented all other values same as "main:  init.."
+
+
+Here is the release run
 ```
 $ cargo run --release
-      Finished release [optimized] target(s) in 0.00s
-      Running `/home/wink/prgs/rust/myrepos/exper-refcell/target/release/exper-refcell`
-s1: 0x7ffca864e5c8
-self: 0x7ffca864e5c8 v1: 123
-self: 0x7ffca864e5c8 v1: 123
-Success
-Drop S1 0x7ffca864e5c8
+   Compiling exper-refcell v0.1.0 (/home/wink/prgs/rust/myrepos/exper-refcell)
+    Finished release [optimized] target(s) in 0.16s
+     Running `target/release/exper-refcell`
+offset_of S1.this 0
+offset_of S1.v1 16
+new:- 0x7ffdea466740 S1 { &this=0x7ffdea466740 this=None &v1=0x7ffdea466750 v1=123 }
+main:+ &owner_s1=0x7ffdea4666f8 owner_s1=0x55a1fbe22af0 S1 { &this=0x55a1fbe22af0 this=None &v1=0x55a1fbe22b00 v1=123 }
+main:  initialize owner_s1: 0x55a1fbe22af0 S1 { &this=0x55a1fbe22af0 this=0x55a1fbe22af0 &v1=0x55a1fbe22b00 v1=123 sc=1 wc=1 }
+main:  create    owner2_s1: 0x55a1fbe22af0 S1 { &this=0x55a1fbe22af0 this=0x55a1fbe22af0 &v1=0x55a1fbe22b00 v1=123 sc=2 wc=1 }
+main:  owner_s1.v1_via_this=123
+main:- owner_s1: 0x55a1fbe22af0 S1 { &this=0x55a1fbe22af0 this=0x55a1fbe22af0 &v1=0x55a1fbe22b00 v1=123 sc=2 wc=1 }
+Drop S1 0x55a1fbe22af0 S1 { &this=0x55a1fbe22af0 this=0x55a1fbe22af0 &v1=0x55a1fbe22b00 v1=123 sc=0 wc=0 }
 ```
 
 ## License
--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,113 @@
-use core::panic;
-use std::{cell::RefCell, mem::MaybeUninit, ptr::addr_of_mut};
+use std::{
+    cell::RefCell,
+    fmt::Display,
+    rc::{Rc, Weak},
+};
+
+use memoffset::offset_of;
 
 const V1_DEFAULT: i32 = 123;
 
-struct S1<'a> {
-    this: RefCell<&'a Self>,
-    v1: i32
+struct S1 {
+    this: RefCell<Option<Weak<Self>>>,
+    v1: i32,
 }
 
-impl<'a> Drop for S1<'a> {
+impl Display for S1 {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let binding = self.this.borrow();
+        if let Some(binding_as_ref) = binding.as_ref() {
+            write!(
+                f,
+                "{self:p} S1 {{ &this={:p} this={:p} &v1={:p} v1={} sc={} wc={} }}",
+                &self.this,
+                (*binding_as_ref).as_ptr(),
+                &self.v1,
+                self.v1,
+                Weak::strong_count(binding_as_ref),
+                Weak::weak_count(binding_as_ref)
+            )
+        } else {
+            write!(
+                f,
+                "{self:p} S1 {{ &this={:p} this=None &v1={:p} v1={} }}",
+                &self.this, &self.v1, self.v1
+            )
+        }
+    }
+}
+
+impl Drop for S1 {
     fn drop(&mut self) {
-        println!("Drop S1 {self:p}");
+        println!("Drop S1 {self}");
     }
 }
 
 #[allow(unused)]
-impl<'a> S1<'a> {
+impl S1 {
     #[inline(never)]
     fn new() -> Self {
-        let mut s1_uninit = MaybeUninit::<S1>::uninit();
-        let s1_mut_ptr = s1_uninit.as_mut_ptr();
-
-        let s1 = unsafe {
-            let s1p = &*s1_mut_ptr;
-            let this = RefCell::new(s1p);
-
-            addr_of_mut!((*s1_mut_ptr).this).write(this);
-            addr_of_mut!((*s1_mut_ptr).v1).write(V1_DEFAULT);
-
-            s1_uninit.assume_init()
+        let s1 = Self {
+            this: RefCell::new(None),
+            v1: V1_DEFAULT,
         };
-        println!("s1: {:p}", &s1);
+        println!("new:- {s1}");
 
-        let v1 = s1.v1_via_this();
-        if v1 == V1_DEFAULT {
-            // This has always succeeded!
-            return s1;
-        }
-        panic!();
+        s1
+    }
+
+    fn add(&mut self, val: i32) {
+        self.v1 += val;
     }
 
     #[inline(never)]
     fn v1_via_this(&self) -> i32 {
-        let v1 = self.this.borrow().v1;
-        println!("self: {self:p} v1: {v1}");
+        //println!("v1_via_this:+ self={self}");
+        let binding = self.this.borrow();
+        let v1 = match binding.as_ref() {
+            Some(binding_as_ref) => {
+                let s1_ptr = Weak::clone(binding_as_ref).as_ptr();
+                unsafe { (*s1_ptr).v1 }
+            }
+            None => {
+                println!("v1_via_this:  this is None");
+                panic!();
+            }
+        };
+
+        //println!("v1_via_this:- v1={v1}");
         v1
     }
 }
 
 #[inline(never)]
 fn main() {
-    let s1 = S1::new();
-    let v1 = s1.v1_via_this();
-    if v1 == V1_DEFAULT {
-        println!("Success");
-        return;
-    }
-    println!("Failed");
+    //println!("size_of S1={}", size_of::<S1>());
+    //println!(
+    //    "size_of RefCell<Option<Weak<S1>>>={}",
+    //    size_of::<RefCell<Option<Weak<S1>>>>()
+    //);
+    //println!("size_of i32={}", size_of::<i32>());
+    //assert!(size_of::<S1>() >= size_of::<RefCell<Option<Weak<S1>>>>() + size_of::<i32>());
+
+    println!("offset_of S1.this {}", offset_of!(S1, this));
+    println!("offset_of S1.v1 {}", offset_of!(S1, v1));
+
+    // Allocate owner_s1
+    let owner_s1: Rc<S1> = Rc::new(S1::new());
+    println!("main:+ &owner_s1={:p} owner_s1={owner_s1}", &owner_s1);
+
+    // Initialize owner_s1.this
+    *owner_s1.this.borrow_mut() = Some(Rc::downgrade(&owner_s1));
+    println!("main:  initialize owner_s1: {owner_s1}");
+
+    // Create a second strong reference
+    let owner2_s1 = Rc::clone(&owner_s1);
+    println!("main:  create    owner2_s1: {owner2_s1}");
+
+    // Invoke method v1_via_this
+    println!("main:  owner_s1.v1_via_this={}", owner_s1.v1_via_this());
+
+    // Invoke method p
+    println!("main:- owner_s1: {owner_s1}");
 }
